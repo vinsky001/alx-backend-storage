e0f1998cc759089ebb44942aca693c2a0d62b08f
vinsky001 <ekibet544@gmail.com>
2023-10-11 19:17:05 +0000

    Fix typo

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- create a table users--
 
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
