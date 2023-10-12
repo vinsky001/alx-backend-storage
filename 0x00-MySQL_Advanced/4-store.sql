ca67e84156b3cb01c29e6b606058c9f6b74effb5
vinsky001 <ekibet544@gmail.com>
2023-10-12 11:36:37 +0000

    QL script that creates a trigger that decreases the quantity of an item after adding a new order.

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -6,9 +6,15 @@ CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
+    DECLARE item_id INT;
+    DECLARE order_quantity INT;
+
+    - Get the item_id and order_quantity from the inserted order
+    SELECT NEW.item_id, NEW.quantity INTO item_id, order_quantity;
+
     UPDATE items
-    SET quantity = quantity - 1
-    WHERE name = NEW.item_name;
+    SET quantity = quantity - order_quantity
+    WHERE id = item_id;
 END//
 
 DELIMITER;
