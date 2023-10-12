e6059761a680b17093971af47688a7c55a5a9fef
vinsky001 <ekibet544@gmail.com>
2023-10-12 11:44:28 +0000

    SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- create a trigger that resets the attribute valid_email only when the email has been changed.
+
+CREATE OR REPLACE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;q
