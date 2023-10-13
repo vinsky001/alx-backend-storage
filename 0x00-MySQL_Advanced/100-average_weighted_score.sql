fd607ab175a4fbf6f0592f30073061a78ec3f56d
vinsky001 <ekibet544@gmail.com>
2023-10-13 05:40:57 +0000

    SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,28 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE total_score DECIMAL(10, 2);
+    DECLARE total_weight DECIMAL(10, 2);
+
+    -- Initialize variables to store the total score and total weight
+    SET total_score = 0;
+    SET total_weight = 0;
+
+    -- Calculate the weighted average for the user
+    INSERT INTO user_weighted_scores (user_id, average_weighted_score)
+    SELECT
+        user_id,
+        SUM(score * weight) / SUM(weight)
+    FROM
+        scores
+    WHERE
+        user_id = user_id;
+
+    -- Commit the transaction
+    COMMIT;
+END$$
+
+DELIMITER ;
